ad7b1d799460d5f2d6701f42ca31da41497cad4d
quocgiau <quocgiau61s@gmail.com>
2023-10-12 15:11:00 +0700

    first commit

--- src/lop_va_doi_tuong/bai_tap/bai1/QuadraticEquation.java
+++ src/lop_va_doi_tuong/bai_tap/bai1/QuadraticEquation.java
@@ -1,2 +1,91 @@
-package lop_va_doi_tuong.bai_tap.bai1;public class QuadraticEquation {
+package lop_va_doi_tuong.bai_tap.bai1;
+
+import java.util.Scanner;
+
+// Xây dựng lớp QuadraticEquation (Phương trình bậc hai)
+public class QuadraticEquation {
+    private double a;
+    private double b;
+    private double c;
+
+    public QuadraticEquation() {
+    }
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    @Override
+    public String toString() {
+        return "QuadraticEquation{" +
+                "a=" + a +
+                ", b=" + b +
+                ", c=" + c +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        QuadraticEquation exp = new QuadraticEquation();
+        System.out.println("Nhập a");
+        double a = scanner.nextDouble();
+        System.out.println("Nhập b");
+        double b = scanner.nextDouble();
+        System.out.println("Nhập c");
+        double c = scanner.nextDouble();
+        if (a != 0) {
+            exp = new QuadraticEquation(a, b, c);
+            if (exp.getDiscriminant() > 0) {
+                System.out.println("Phương trình có 2 nghiệm là: x1 = " + exp.getRoot1() + " x2 = " + exp.getRoot2());
+            } else if (exp.getDiscriminant() == 0) {
+                System.out.println("Phương trình có nghiệm kép x = " + exp.getRoot());
+            } else {
+                System.out.println("Phương trình vô nghiệm");
+            }
+        } else {
+            System.out.println("Vui lòng nhập số khác 0");
+        }
+    }
+
+    public double getDiscriminant() {
+        return this.b * this.b - 4 * this.a * this.c;
+    }
+
+    public double getRoot1() {
+        return ((-this.b + Math.sqrt(getDiscriminant())) / (2 * this.a));
+    }
+
+    public double getRoot2() {
+        return ((-this.b - Math.sqrt(getDiscriminant())) / (2 * this.a));
+    }
+
+    public double getRoot() {
+        return -this.b / (2 * this.a);
+    }
 }
--- src/lop_va_doi_tuong/bai_tap/bai2/Fan.java
+++ src/lop_va_doi_tuong/bai_tap/bai2/Fan.java
@@ -1,2 +1,106 @@
-package lop_va_doi_tuong.bai_tap.bai2;public class Fan {
+package lop_va_doi_tuong.bai_tap.bai2;
+
+import java.util.Scanner;
+
+public class Fan {
+    private final int SLOW = 1;
+    private final int MEDIUM = 2;
+    private final int FAST = 3;
+    private int speed = SLOW;
+    private boolean status = false;
+    private double radius = 5;
+    private String color = "blue";
+
+    public Fan() {
+    }
+
+    public Fan(int speed, boolean status, double radius, String color) {
+        this.speed = speed;
+        this.status = status;
+        this.radius = radius;
+        this.color = color;
+    }
+
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setOn(boolean status) {
+        this.status = status;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String toString() {
+        if (getStatus()) {
+            if(getSpeed() == SLOW){
+                return "Fan{" +
+                        "speed= " + "SLOW" +
+                        ", fan is on" +
+                        ", radius=" + radius +
+                        ", color='" + color + '\'' +
+                        '}';
+            }else if (getSpeed() == MEDIUM) {
+                return "Fan{" +
+                        "speed= " + "MEDIUM" +
+                        ", fan is on" +
+                        ", radius=" + radius +
+                        ", color='" + color + '\'' +
+                        '}';
+            }else if (getSpeed()== FAST){
+                return "Fan{" +
+                        "speed= " + "FAST" +
+                        ", fan is on" +
+                        ", radius=" + radius +
+                        ", color='" + color + '\'' +
+                        '}';
+            }else {
+                return "Fan{" +
+                        "speed= " + null +
+                        "fan is on" +
+                        ", radius=" + radius +
+                        ", color='" + color + '\'' +
+                        '}';
+            }
+        } else {
+            return "Fan{" +
+                    "fan is off" +
+                    ", radius=" + radius +
+                    ", color='" + color + '\'' +
+                    '}';
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Fan fan1 = new Fan(3, true, 10, "yellow");
+        Fan fan2 = new Fan(2, false, 5, "blue");
+        Fan fan3 = new Fan();
+        System.out.println(fan1);
+        System.out.println(fan2);
+        System.out.println(fan3);
+    }
+
 }
