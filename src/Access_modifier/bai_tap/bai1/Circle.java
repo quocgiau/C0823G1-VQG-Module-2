23e15c3154cd9721352cdb2aff3ef25c6f3b18ed
quocgiau <quocgiau61s@gmail.com>
2023-10-17 14:05:44 +0700

    first commit

--- src/Access_modifier/bai_tap/bai1/Circle.java
+++ src/Access_modifier/bai_tap/bai1/Circle.java
@@ -4,9 +4,8 @@ public class Circle {
     private double radius = 1.0;
     private String color = "red";
 
-    public Circle(double radius, String color) {
+    public Circle(double radius) {
         this.radius = radius;
-        this.color = color;
     }
 
     public Circle() {
@@ -16,27 +15,12 @@ public class Circle {
         return radius;
     }
 
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
     public double getArea() {
         return Math.pow(getRadius(), 2) * Math.PI;
     }
 
     @Override
     public String toString() {
-        return
-                "Bán kính " + getRadius() +
-                        ", Màu " + getColor() +
-                        ", Diện tích " + getArea();
+        return "Bán kính " + getRadius() + ", Màu '" + color + "', Diện tích " + getArea();
     }
 }
--- src/Access_modifier/bai_tap/bai1/TestCircle.java
+++ src/Access_modifier/bai_tap/bai1/TestCircle.java
@@ -2,7 +2,9 @@ package Access_modifier.bai_tap.bai1;
 
 public class TestCircle {
     public static void main(String[] args) {
-        Circle circle1 = new Circle(5.0, "blue"); //Khởi tạo đối tượng có bán kính là 5 màu xanh
+        Circle circle1 = new Circle(5.0); //Khởi tạo đối tượng có bán kính là 5
         System.out.println(circle1);
+        System.out.println("Bán kính " + circle1.getRadius());
+        System.out.println("Diện tích " + circle1.getArea());
     }
 }
